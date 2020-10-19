8abf0ef4ab7ae2f55133544e2921f0a46e25d8d4
alessio <alessio.cirillo.1995@gmail.com>
2020-10-19 17:49:15 +0200

    ordine alfabetico e posizionamento

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 // ARRAY
 var lista = [ 'Cirillo', 'Curci', 'Duzioni', 'Caronti',];
 
-
 // RICHISESTA UTENTE
-var cognomeUtente = prompt ('Inserire cognome').toLocaleLowerCase();
+var cognomeUtente = prompt ('Inserire cognome');
 
 // INSERIMENTO IN ARRAY
 lista.push(cognomeUtente);
-console.log(lista);
 
 //ORDINE ALFABETICO
 lista.sort();
 console.log(lista);
+
+//POSIZIONE
+console.log(lista.indexOf(cognomeUtente) + 1);
