ea63df0fa7a147200a8b88d9ad21bc9480173c8b
alessio <alessio.cirillo.1995@gmail.com>
2020-10-19 16:00:50 +0200

    array e prompt

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// ARRAY
+var lista = [ 'Cirillo', 'Curci', 'Duzioni', 'Caronti',];
+
+
+// RICHISESTA UTENTE
+var cognomeUtente = prompt ('Inserire cognome').toLocaleLowerCase;
+
+// INSERIMENTO IN ARRAY
+lista.push(cognomeUtente);
+console.log(lista);
